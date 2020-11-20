d687df9e73b5b8fcff6eea453c585dcaab49d982
Sebastian Amaya <sebasamaya.gomez@gmail.com>
2020-11-20 12:57:37 -0500

    Configuracion de docker y apache kafka

--- mvc/Views/routes.js
+++ mvc/Views/routes.js
@@ -1,15 +1,32 @@
 const express= require('express');
 const app=express();
-
+const kafka=require('kafka-node');
 const {Shipping}=require('./Sequelize');
 const {Country}=require('./Sequelize');
+
+//configuracion de kafka
+//const client=new kafka.KafkaClient({kafkaHost:'127.0.0.1:9092'});
+//consumer
+//var producer=new kafka.Producer(client);
 //mostrar las ubicaciones disponibles
 app.get('/shipping',(req,res)=>{
+    var paises="";
     Country.findAll()
     .then(countries=>{
-        res.status(200).json({ countries });
+        countries.forEach(country=>{
+            console.log(country.pais);
+            if(country.pais!="Rusia"){
+                paises+=" "+country.pais+" , ";
+            }else{
+                paises+=country.pais+" ";
+            }
+            /*producer.send(
+            [{topic:"shipping",messages:`Pais : ${country.pais}`}],
+            function(err,data){}
+            );*/
+        })
+        res.status(200).json({ paises });
     });
-
 });
 //crear el envio
 app.post('/shipping/new',async(req,res)=>{
@@ -22,6 +39,10 @@ app.post('/shipping/new',async(req,res)=>{
         direccion_destino:req.body.direcciondestino,
         telefono_destinatario:req.body.telefonodestinatario
     }).then(shipping=>{
+        /*producer.send(
+            [{topic:"shipping-create",messages:"Envio creado correctamente"}],
+            function(err,data){}
+            );*/
         res.json({'Status':200,'res':'Envio creado correctamente'});
     });
 
@@ -35,8 +56,16 @@ app.post('/shipping/cal',async(req,res)=>{
     if(paisd){
         let tarifa=paisd.tarifa_pais;
         let pago=(peso*5000)+tarifa;
+        /*producer.send(
+            [{topic:"shipping-calculo",messages:`El pago a realizar es de : ${pago}`}],
+            function(err,data){}
+            );*/
         res.status(200).json({ 'Pago': `El pago a realizar es de : ${pago}` });
     }else{
+        /*producer.send(
+            [{topic:"shipping-calc",messages:"Error el pais no tiene covertura"}],
+            function(err,data){}
+            );*/
         res.status(401).json({ error: 'Error el pais no tiene covertura' });
     }
 });
@@ -47,10 +76,18 @@ app.post('/shipping/vel',async(req,res)=>{
     let total;
     if(velocidad=="rapido"){
         total=(costoEnvio*0.15)+costoEnvio;
-        res.status(200).json({ 'Consto': `El costo total del envio es de : ${total}` });
+        /*producer.send(
+            [{topic:"shipping-velocidad",messages:`El costo total del envio es de : ${total}`}],
+            function(err,data){}
+            );*/
+        res.status(200).json({ 'Costo': `El costo total del envio es de : ${total}` });
     }else{
         total=costoEnvio;
-        res.status(200).json({ 'Consto': `El costo total del envio es de : ${total}` });
+        /*producer.send(
+            [{topic:"shipping-velocidad",messages:`El costo total del envio es de : ${total}`}],
+            function(err,data){}
+            );*/
+        res.status(200).json({ 'Costo': `El costo total del envio es de : ${total}` });
     }
 });
 //actualizar el lugar de destino
@@ -71,9 +108,17 @@ app.put('/shipping/:id',async(req,res)=>{
                 }
             })
         .then(newshipping=>{
+            /*producer.send(
+            [{topic:"shipping-destino",messages:newshipping}],
+            function(err,data){}
+            );*/
             res.json({'Status':200,'res':newshipping})
         })
     }else{
+        /*producer.send(
+            [{topic:"shipping-destino",messages:"El envio no existe"}],
+            function(err,data){}
+            );*/
         res.json({ error: 'El envio no existe' })
     }
 });
